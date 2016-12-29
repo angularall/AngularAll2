3fca616987d4f7476da5656c54ccbff9a2861c2d
angularall <ertamilselvans@gmail.com>
2016-12-30 00:09:54 +0530

    Added examples and directive files
    Simple examples for angular1

--- WebContent/WEB-INF/assets/js/controller/mainController.js
+++ WebContent/WEB-INF/assets/js/controller/mainController.js
@@ -21,7 +21,7 @@
 	A1ExampleCtrl.$inject=['$scope','dataService'];
 	function A1ExampleCtrl($scope,dataService){
 		$scope.a1Topics=loadA1Topics();
-		$scope.e1TabList=['Example','Html','JavaScript','Result','Steps','Mistakes']
+		$scope.e1TabList=['Example','Html','JavaScript','All Steps','Mistakes','VideoHelp','Editor']
 	}
 
 	angular.module('aaApp').controller('angular1cmpctrl',angular1cmpctrl);
